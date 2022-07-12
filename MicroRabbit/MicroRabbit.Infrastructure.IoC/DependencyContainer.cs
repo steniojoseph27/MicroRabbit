5b5cff432d576750f28314b740ac38884d794fe0
Stenio Joseph <stenio.joseph@encora.com>
2022-07-11 23:50:18 -0400

    Initial Commit of Infrastructure Dependency Project

---
+++ MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
